fa18dc98035703ed9ef551d5b797856ebf8a7633
Mina <mukaram0709@gmail.com>
2019-02-01 18:11:36 -0800

    commit to = reOrganize annotation and close driver

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -14,7 +15,7 @@ import java.util.concurrent.TimeUnit;
 
 public class LoginTests {
     WebDriver driver;
-    @BeforeClass
+    @BeforeMethod
     public void setUp(){
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
@@ -22,11 +23,15 @@ public class LoginTests {
 
     }
     @Test
-    public void loginTest1(){
+    public void loginTest1() {
         driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
         driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
         driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test" + Keys.ENTER);
         Assert.assertEquals(driver.getTitle(), "Web Orders");
+    }
+  @AfterMethod
+    public void cleanUp(){
+        driver.close();
 
     }
 }
